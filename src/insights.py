32be36b047ba7f753faa233aa4fc54d867c4e725
Christian Lessa <christianclessa@gmail.com>
2022-09-13 14:58:11 -0300

    function filter_by_job_type completed

--- src/insights.py
+++ src/insights.py
@@ -10,21 +10,14 @@ def get_unique_job_types(path):
 
 
 def filter_by_job_type(jobs, job_type):
-    """Filters a list of jobs by job_type
+    list_jobs = []
+    for job in jobs:
+        if job["job_type"] == job_type:
+            list_jobs.append(job)
+    return list_jobs
 
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
-    return []
+    # Ou em uma linha
+    # return [job for job in jobs if job["job_type"] == job_type]
 
 
 def get_unique_industries(path):
