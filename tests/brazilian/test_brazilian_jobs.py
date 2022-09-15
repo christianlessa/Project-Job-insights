7726f3289345874561b8d5db9e4e92448afdefdb
Christian Lessa <christianclessa@gmail.com>
2022-09-15 13:06:34 -0300

    function test_brazilian_jobs.py completed

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,7 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    mock = {"salary": "3000", "title": "Motorista", "type": "full time"}
+
+    assert mock in read_brazilian_file("tests/mocks/brazilians_jobs.csv")
